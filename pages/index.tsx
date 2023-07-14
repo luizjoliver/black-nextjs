ccbb134f1dc01db92c79547cb6b62aae3035ed8f
Luiz fellipe Joliver Gonçalves e silva <106269210+luizjoliver@users.noreply.github.com>
2023-07-14 11:52:52 -0300

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,9 @@ const Home: NextPage = () => {
         <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
+      <h2>
+        Created By : Luiz fellipe Joliver
+      </h2>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint error consectetur modi odio obcaecati tempore dignissimos assumenda, eum reiciendis dolores magni autem at debitis voluptas. Rem ipsa deleniti autem dignissimos.
         </p>
